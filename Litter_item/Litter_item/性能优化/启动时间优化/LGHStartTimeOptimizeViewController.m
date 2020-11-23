7fc0345753ea4ce07eab94fd8157fe1d7d7302e1
smoothv <450929738@qq.com>
2020-11-23 14:13:23 +0800

    1. 整理文件

--- Litter_item/Litter_item/性能优化/启动时间优化/LGHStartTimeOptimizeViewController.m
+++ Litter_item/Litter_item/性能优化/启动时间优化/LGHStartTimeOptimizeViewController.m
@@ -30,7 +30,11 @@
         },
         @{
             @"title":@"抖音品质建设 - iOS启动优化《原理篇》",
-            @"url":@"https://juejin.im/post/6887741815529832456"}
+            @"url":@"https://juejin.im/post/6887741815529832456"},
+        @{
+            @"title":@"基于二进制文件重排的解决方案 APP启动速度提升超15%",
+            @"url":@"https://mp.weixin.qq.com/s?__biz=MzI1MzYzMjE0MQ==&mid=2247485101&idx=1&sn=abbbb6da1aba37a04047fc210363bcc9&scene=21&token=2051547505&lang=zh_CN"
+        }
     ]];
     
     self.dataSource = [[LGHBaseTableDataSource alloc]initWithIdentifier:@"UITableViewCell" configureBlock:^(UITableViewCell* _Nonnull cell, NSDictionary*  _Nonnull model, NSIndexPath * _Nonnull indexPath) {
