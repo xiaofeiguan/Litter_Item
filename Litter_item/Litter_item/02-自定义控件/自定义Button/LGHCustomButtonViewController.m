173867e43d2da969a676b34477e9476296e616b0
smoothv <450929738@qq.com>
2020-11-26 22:37:46 +0800

    删除多余代码

--- Litter_item/Litter_item/02-自定义控件/自定义Button/LGHCustomButtonViewController.m
+++ Litter_item/Litter_item/02-自定义控件/自定义Button/LGHCustomButtonViewController.m
@@ -6,9 +6,7 @@
 //
 
 #import "LGHCustomButtonViewController.h"
-#import "XJVerticalButton.h"
 @interface LGHCustomButtonViewController ()
-@property(nonatomic,strong) XJVerticalButton *button01;
 @end
 
 @implementation LGHCustomButtonViewController
@@ -16,16 +14,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
-    self.button01 = [[XJVerticalButton alloc]initWithImageY:5 titleImagePadding:5 titleHeight:10 imageSize:CGSizeMake(25, 25)];
-    
-    self.button01.backgroundColor = [UIColor lightGrayColor];
-    self.button01.frame = CGRectMake(0, 80, 40, 40);
-    [self.button01 setImage:[UIImage imageNamed:@"tabbar_help"] forState:UIControlStateNormal];
-    [self.button01 setTitle:@"帮助" forState:UIControlStateNormal];
-    self.button01.titleLabel.font = [UIFont systemFontOfSize:10.0];
-    [self.view addSubview:self.button01];
-    
-
     
 }
 
