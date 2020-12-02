87fe80a9f3eeb3ac9ba5c9ede93d94fbc16c4365
smoothv <450929738@qq.com>
2020-12-02 18:55:25 +0800

    add 笔试题

---
+++ Litter_item/Litter_item/笔试/LGHWrittenExaminationViewController.m
@@ -0,0 +1,49 @@
+//
+//  LGHWrittenExaminationViewController.m
+//  Litter_item
+//
+//  Created by 小肥观 on 2020/12/2.
+//
+
+#import "LGHWrittenExaminationViewController.h"
+
+@interface LGHWrittenExaminationViewController ()
+
+@end
+
+@implementation LGHWrittenExaminationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self test01];
+}
+
+/*
+ * 输出的结果是？
+ * 解析见：test01.png
+ */
+-(void)test01{
+    int a [3] = {1,2,3};
+    int *ptr = (int*) (&a + 1);
+    printf ("%d,%d",*(a + 1) ,*(ptr - 1));
+}
+
+/*
+ * 可能输出的结果是？
+ */
+-(void)test02{
+    
+}
+
+/*
+ * 冒泡排序
+ */
+
+
+
+
+
+
+
+@end
