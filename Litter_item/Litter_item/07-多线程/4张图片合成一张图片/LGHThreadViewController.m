7894bb2d584a5ff036e38057ae9ff2817b2ca8e9
smoothv <450929738@qq.com>
2020-12-18 18:58:08 +0800

    多张图片合成一张图片

---
+++ Litter_item/Litter_item/07-多线程/4张图片合成一张图片/LGHThreadViewController.m
@@ -0,0 +1,31 @@
+//
+//  LGHThreadViewController.m
+//  Litter_item
+//
+//  Created by 小肥观 on 2020/12/18.
+//
+
+#import "LGHThreadViewController.h"
+
+@interface LGHThreadViewController ()
+
+@end
+
+@implementation LGHThreadViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
